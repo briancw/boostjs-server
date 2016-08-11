108d4e49bce66c474dfa718e656341240bd9708a
Brian Whicheloe <brian@snapshot.is>
2016-08-11 11:01:58 -0700

    Add boost, package son, and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# BOOST JS 🚀 #
+```
+npm install node-boost-beta
+
+const boost = require('node-boost-beta');
+const port = 3000;
+const app = boost.app;
+app.get('/', function(req, res) {
+    res.send('It Works!');
+});
+boost.launch(port, err => {
+    console.log('Listening on port ' + port);
+});
+```
+
+## Notable Npm Modules ##
+### Core: ###
+  * thinky: RethinkDB ORM
+  * socket.io: socket connections between server and client with multiplexing, fallbacks, auto-reconnect, and other useful features
+
+## Project Requirements ##
+This project requires Rethinkdb to be installed on your machine
+  https://www.rethinkdb.com/docs/install/
+
+Additionally, you will need node and npm. Current recommend version is 6.x
---
+++ node-boost.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const hash = require('object-hash');
+const spdy = require('spdy');
+
+const server_opts = {
+    spdy: {
+        protocols: ['http/1.1'],
+        plain: true,
+    },
+    // key: fs.readFileSync('./server.key'),
+    // cert: fs.readFileSync('./server.crt'),
+};
+
+const server = spdy.createServer(server_opts, app);
+const io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// app.use(require('connect-history-api-fallback')());
+
+module.exports = {
+    app: app,
+    io: io,
+    launch(port) {
+        server.listen(port, err => {
+            this.helpers.handle(err);
+            console.log('Listening on port: ' + port);
+        });
+    },
+    publish(path, model, query = {}) {
+        const channel = io.of(path);
+
+        channel.on('connection', function(socket) {
+            console.log('new connection to ' + path);
+
+            socket.on('verify_data', data_hash => {
+                console.log('verify data request ' + data_hash);
+
+                model.run().then(data => {
+                    let current_hash = hash(JSON.stringify(data));
+                    // console.log('Current Data Hash: ' + current_hash);
+
+                    let is_valid = (data_hash === current_hash);
+                    socket.emit('verify_response', {valid: is_valid});
+                });
+            });
+
+            socket.on('get_data', function() {
+                console.log('get data request');
+
+                model.filter(query).run().then(data => {
+                    let current_hash = hash(data);
+                    socket.emit('data_response', {data: data, hash: current_hash});
+                });
+            });
+        });
+
+        model.changes().then(function(changes) {
+            changes.each(function(error, doc) {
+                if (error) {
+                    // console.log(error);
+                }
+
+                let change_type;
+                if (doc.isSaved() === false) {
+                    // console.log('document deleted');
+                    change_type = 'delete';
+                } else if (doc.getOldValue() === null) {
+                    // console.log('new document');
+                    change_type = 'insert';
+                } else {
+                    // console.log('document update');
+                    change_type = 'update';
+                }
+
+                // console.log(change_type);
+                channel.emit('update', change_type, doc);
+            });
+        }).error(function(error) {
+            console.log(error);
+        });
+    },
+    helpers: {
+        handle: function(err) {
+            if (err) {
+                throw err;
+            }
+        },
+    },
+};
