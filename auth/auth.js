e2135e123a58b6915418ec5a068976adc04154a3
Jacob Smith <messagethesmith@gmail.com>
2016-09-16 15:07:04 -0700

    Update server to new auth system. auth.js attaches auth status to req object. Added login and logout functions to BoostServer

--- auth/auth.js
+++ auth/auth.js
@@ -1,25 +1,48 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const secret_key = process.env.JWT_SECRET;
 
 module.exports = {
-    auth: function(req, res, next) {
-        // console.log(req)
-        if (req.body.token) {
-            let token = req.body.token;
-            // console.log(token);
-            jwt.verify(token, secret_key, function(err, decoded) {
-                if (err) {
-                    console.log('Not Authorized');
-                    // res.status('401');
-                } else {
-                    // console.log(decoded);
-                    req.user_id = decoded.user_id;
+        auth: function(req, res, next) {
+            if (req.body.token) {
+                let token = req.body.token;
+
+                jwt.verify(token, secret_key, function(err, decoded) {
+                    console.log('######');
+                    console.log(Date.now());
+                    console.log(decoded.invalidationDate);
+                    console.log(Date.now() >= decoded.invalidationDate);
+                    console.log('######');
+
+                    // If something went wrong
+                    if (err) {
+                        console.log('Error in server auth');
+                        req.userId = null;
+                        req.authStatus = null;
+                    // If decoded data exists
+                    } else {
+                        req.authStatus = decoded.authStatus;
+                        let sessionFileName = './.sessions/' + token.substr(token.lastIndexOf('.') + 1, 8) + '.s';
+
+                        // If token is too old or invalidated
+                        if (!decoded.invalidationDate ||
+                            Date.now() >= decoded.invalidationDate ||
+                            fs.existsSync(sessionFileName)) {
+
+                            console.log('Not Authorized');
+                            req.authStatus = null;
+                        } else {
+                            console.log('Authorized through server');
+                            req.authStatus = decoded.authStatus;
+                        }
+                    }
                     next();
-                }
-            });
-        } else {
-            // console.log('No token');
-            next();
-        }
-    },
+                });
+            } else {
+                console.log('No token supplied to server auth');
+                console.log(req.body);
+                next();
+            }
+        },
+    // };
 };
--- node-boost.js
+++ node-boost.js
@@ -5,6 +5,10 @@ const helmet = require('helmet');
 const spdy = require('spdy');
 const BoostRethink = require('./store/adapters/boost-rethink');
 const boostAuth = require('./auth/auth');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const secret_key = process.env.JWT_SECRET;
 
 // Cache options
 const RedisCache = require('./cache/adapters/redis');
@@ -17,18 +21,19 @@ app.use(boostAuth.auth);
 app.use(require('connect-history-api-fallback')());
 
 class BoostServer {
-    constructor(server_opts) {
-        this.server = spdy.createServer(server_opts, app);
+    constructor(serverOpts) {
+        this.server = spdy.createServer(serverOpts, app);
         this.app = app;
         this.io = this.io;
-        this.cacheOpts = server_opts.cache;
+        this.serverOpts = serverOpts;
+
 
         this.caches = {
             redis: RedisCache,
             array: ArrayCache,
         };
 
-        this.cache = this.caches[this.cacheOpts.type];
+        this.cache = this.caches[this.serverOpts.cache.type];
 
         let io = require('socket.io').listen(this.server);
         io.set('transports', ['websocket']);
@@ -58,6 +63,73 @@ class BoostServer {
             cache: this.cache,
             cacheOpts: this.cacheOpts,
         }).start(this);
+    }
+
+    getLoginToken(user, authStatus) {
+        let invalidationDate = Date.now() + this.serverOpts.jwt.lifetime;
+
+        let token = jwt.sign({
+            userId: user.id,
+            invalidationDate: invalidationDate,
+            authStatus: authStatus,
+        },
+            secret_key
+        );
+
+        return {
+            token,
+            invalidationDate,
+        };
+    }
+
+    logoutToken(token) {
+        console.log('typeof token');
+        console.log(typeof token);
+        if (!token || typeof token !== 'string') {
+            return false;
+        }
+        let fileName = token.substr(token.lastIndexOf('.') + 1, 8);
+        console.log('wrote session');
+        fs.writeFileSync('./.sessions/' + fileName + '.s', '', 'utf8');
+        return true;
+    }
+
+    // Thought I'd start on the tasking stuff. Dunno if this works but it's not used anywhere yet.
+    // Will the intervals stay in the event loop after the server quits?
+    task(name, handler, frequency, opts) {
+        opts = opts || {};
+        let id;
+
+        if (typeof handler !== 'object' || typeof name !== 'string' || typeof frequency !== 'number') {
+            throw 'Incorrect params';
+        }
+
+        if (this.tasks[name]) {
+            throw 'Task already exists';
+        }
+
+        if (opts.once) {
+            id = setTimeout(handler, frequency);
+        } else {
+            id = setInterval(handler, frequency);
+        }
+
+        this.tasks[name] = {
+            id,
+            timeout: this.opts.once,
+        };
+    }
+
+    cancelTask(name) {
+        let taskInfo = this.tasks[name];
+        if (!taskInfo) {
+            return;
+        }
+        if (taskInfo.timeout) {
+            clearTimeout(taskInfo.id);
+        } else {
+            clearInterval(taskInfo.id);
+        }
     }
 }
 
